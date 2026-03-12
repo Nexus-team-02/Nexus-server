60a0e24bbd5c79cb4b3b554146950d405e661cdd
minzix <minzikx@sookmyung.ac.kr>
2026-03-12 09:42:34 +0900

    feat: 각 팀 당 처음에 생성된 Flow 조회 추가 #161

--- src/main/java/pingpong/backend/domain/flow/repository/FlowRepository.java
+++ src/main/java/pingpong/backend/domain/flow/repository/FlowRepository.java
@@ -5,10 +5,23 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import pingpong.backend.domain.flow.Flow;
 
 public interface FlowRepository extends JpaRepository<Flow, Long> {
 
 	List<Flow> findByTeamId(Long teamId);
+
+	@Query("""
+		select f
+		from Flow f
+		where f.team.id in :teamIds
+		  and f.id = (
+		    select min(f2.id)
+		    from Flow f2
+		    where f2.team.id = f.team.id
+		  )
+		""")
+	List<Flow> findFirstFlowPerTeam(@Param("teamIds") List<Long> teamIds);
 }
